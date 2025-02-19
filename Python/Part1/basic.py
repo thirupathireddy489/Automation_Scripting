68f41c5159fe895feac3cf0203cbcb5814151e76
thirupathireddy489 <thirupathireddy.g489@gmail.com>
2025-02-19 13:21:48 +0530

    Basic - About Python

---
+++ Python/Part1/basic.py
@@ -0,0 +1,44 @@
+#python:
+
+#Created by Guido van Rossum
+#python - Feb 1991
+#python2 - Oct 2000 to Jan 2020 - No further updates or support are provided for this version
+#python3 - Dec 2008 - It has been continuously improved with new features and optimizations
+
+#It is written in the C programming langauge
+
+# Python is open source
+#interpreter based langauge - it executes the program line by line, interpreter name is python pythonc
+#easy to learn - it is simple - easy to understand like english
+#easy to maintain the code
+#high level langauge
+#cross plotform
+
+#supports OOP
+
+#It has community support
+#
+#where can we use python
+
+#Extensive Third-Party Libraries and Frameworks
+#
+#	•	Web development: Django, Flask
+#	•	Data science: pandas, NumPy, scikit-learn
+#	•	Visualization: Matplotlib, Seaborn
+#	•	Automation: Scripting repetitive tasks, Selenium, pyautogui
+#	•	Game development: Pygame, Godot
+#       Networking: Paramico, Scapy
+#       AI, IOT, Devops and cloud computing
+
+
+#Integration with Other Languages
+#Python can be integrated with other languages like:
+#	•	C/C++ (via ctypes, Cython, or SWIG)
+#	•	Java (via Jython)
+#	•	.NET (via IronPython)
+
+# High demand in job market
+
+##compilar - which executes the whole program at a time  -  eg: c language
+
+
